b14059571030a755707c4d8c14c5ec473684eaa7
Алексей Трошин <nightrevan@yandex.ru>
2021-01-19 14:12:07 +0700

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+
+int main()
+{
+
+	std::string word = "cocetkuatol";
+	std::string w = "c", d = "l";
+
+	std::cout << word << '\n';
+
+	std::cout << word.length() << "\n";
+	std::cout << w + d << "\n";
+
+
+	std::cin;
+	return 0;
+
+
+}
+
+
+	
